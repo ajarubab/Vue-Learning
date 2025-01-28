dce748c761a21276822261a9d0211b09d2653f0e
raja kumar prasad <imrajakumarprasad@gmail.com>
2025-01-28 14:07:54 +0530

    vue learning session 1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to New Vue tutorial"/>
+  <MyHome/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyHome from './components/MyHome.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyHome
+  }
+}
+</script>
+
