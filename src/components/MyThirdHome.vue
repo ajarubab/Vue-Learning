1f07164fb20fc3a3fde7d67859488f7bcbe2216e
raja kumar prasad <imrajakumarprasad@gmail.com>
2025-02-04 13:30:36 +0530

    use of Ref

--- src/components/MyThirdHome.vue
+++ src/components/MyThirdHome.vue
@@ -2,6 +2,9 @@
     <h1>My Third Home</h1>
     <h2>{{ msgToThirdHome }}</h2>
     <button v-on:click="getThirdHomeMsg(msgToSecongHome)">Send Msg to Second Home</button>
+    <input type="text" ref="nameInput" />
+    <button v-on:click="getdata1">Click me to type in Red alphabets</button>
+    <button v-on:click="getdata2">Click me to type in Blue alphabets</button>
 </template>
 <script>
 export default {
@@ -13,6 +16,20 @@ export default {
     data() {
         return {
             msgToSecongHome: "Hello from Third Home"
+        }
+    },
+    methods: {
+        getdata1() {
+            this.$refs.nameInput.focus();
+            let val = this.$refs.nameInput.value;
+            console.warn(val);
+            this.$refs.nameInput.style.color = "red"
+        },
+        getdata2() {
+            this.$refs.nameInput.focus();
+            let val = this.$refs.nameInput.value;
+            console.warn(val);
+            this.$refs.nameInput.style.color = "Blue"
         }
     }
 }
