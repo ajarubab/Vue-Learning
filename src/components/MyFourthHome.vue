c29884978b1a23f6a8963546a69a22828352eb90
raja kumar prasad <imrajakumarprasad@gmail.com>
2025-02-26 20:00:49 +0530

    USe of Computed Block

--- src/components/MyFourthHome.vue
+++ src/components/MyFourthHome.vue
@@ -5,21 +5,45 @@
     <div v-bind="$attrs">
         <h2>Apple</h2>
         <h2>Mango</h2>
-        <h2 v-bind="$attrs" >Banana</h2>
+        <h2 v-bind="$attrs">Banana</h2>
         <h2>{{ someData }}</h2>
     </div>
+    <h2>------------------------ Use of Computed Block ----------------------------------<br></h2>
+    <h3>Principal = {{ P }}</h3>
+    <h3>Rate of Interest = {{ R }}</h3>
+    <h3>Time = {{ T }}</h3>
+    <h3>The SI is : {{ getSI }} Rupees only.</h3>
+    <h3>The Total Amout is : {{ getTotalAmount }} Rupees only.</h3>
+    <h2>----------------------------------------------------------<br></h2>
 </template>
 <script>
-export default{
-    name : "MyFourthHome",
+export default {
+    name: "MyFourthHome",
     inheritAttrs: true,
-    props:{
+    data() {
+        return {
+            P: Math.floor(Math.random()*1000),
+            R: Math.floor(Math.random()*10),
+            T: Math.floor(Math.random()*10),
+        }
+    },
+    props: {
         someData: String
+    },
+    computed: {
+        getSI() {
+            let SI = (this.P * this.R * this.T) / 100;
+            return SI;
+        },
+        getTotalAmount() {
+            let Total = (this.P+(this.P * this.R * this.T) / 100);
+            return Total;
+        },
     }
 }
 </script>
 <style scoped>
-h1{
+h1 {
     color: red !important;
 }
 </style>
