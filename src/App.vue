3d7518cbdf546d1e00cd81c771a5209ee25387e1
raja kumar prasad <imrajakumarprasad@gmail.com>
2025-02-27 20:19:39 +0530

    Use of vue life cycle function in random numbering to evaluatate the actual sequence of processing

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,41 @@
   <!-- <MyHome/> -->
   <!-- <MySecondHome/> -->
 
-  <h1>Using the components dianamically</h1>
+  <!-- <h1>Using the components dianamically</h1>
   <button @click="tab = 'HelloWorld'">Show HelloWorld Component content</button><br>
   <button @click="tab = 'MyHome'">Show MyHome Component content</button><br>
   <button @click="tab = 'MySecondHome'">Show MySecondHome Component content</button><br>
   <button @click="tab = 'MyThirdHome'">Show MyThirdHome Component content</button><br>
   <button @click="tab = 'MyFourthHome'">Show MyFourthHome Component content</button><br>
-  <component :is="tab" />
+  <component :is="tab" /> -->
+  <button v-on:click="show = !show">Toggle</button>
+  <Footer v-if="show" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MyHome from './components/MyHome.vue';
-import MySecondHome from './components/MySecondHome.vue';
-import MyThirdHome from './components/MyThirdHome.vue';
-import MyFourthHome from './components/MyFourthHome.vue';
+import Footer from './components/footer.vue';
+
+// import HelloWorld from './components/HelloWorld.vue'
+// import MyHome from './components/MyHome.vue';
+// import MySecondHome from './components/MySecondHome.vue';
+// import MyThirdHome from './components/MyThirdHome.vue';
+// import MyFourthHome from './components/MyFourthHome.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    MyHome,
-    MySecondHome,
-    MyThirdHome,
-    MyFourthHome,
+    // HelloWorld,
+    // MyHome,
+    // MySecondHome,
+    // MyThirdHome,
+    // MyFourthHome,
+    Footer
   },
   data() {
     return {
-      tab: ''
+      tab: '',
+      show: true
     }
   }
 }
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,12 +1,43 @@
 <template>
     <div class="footer">
-        <h1>This is Footer</h1>
-        <h3>CopyRight @LearnWithHarry</h3>
+        <h1 ref="count">{{ count }}. {{greet}}, This is Footer</h1>
+        <button v-on:click="count = count +1">Add</button>
     </div>
 </template>
 <script>
 export default {
-    name: "FooterComponent"
+    name: "FooterComponent",
+    unmounted(){
+        console.log("unMount finished",this.$refs['count']);
+    },
+    updated(){
+        console.log("updation finished",this.$refs['count'].textContent);
+    },
+    mounted(){
+        console.log("mounted finished",this.$el);
+    },
+    created(){
+        console.log("creation finished");
+    },
+    data(){
+        console.log("into data");
+        return {
+            greet : "Hello",
+            count : 0,
+        }
+    },
+    beforeUnmount() {
+        console.log("before unMount",this.$refs['count']);
+    },
+    beforeUpdate() {
+        console.log("before update",this.$refs['count'].textContent);
+    },
+    beforeCreate() {
+        console.log("before created");
+    },
+    beforeMount() {
+        console.log("before Mount",this.$el);
+    },
 }
 </script>
 <style>
